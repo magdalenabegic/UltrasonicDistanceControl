44259ff9535ac68308af09334daf5984ca9deafe
Magdalena Begić <98237744+magdalenabegic@users.noreply.github.com>
2023-07-13 11:39:17 +0200

    Update final.py

--- final.py
+++ final.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 
+# Set the GPIO mode for pin numbering
 GPIO.setmode(GPIO.BOARD)
 
 crvena_sustav = 38
@@ -30,7 +31,7 @@ GPIO.setup(plava, GPIO.OUT)
 GPIO.setup(button_on_off, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 ButtonState = 0
 
-#gumb koji pali/gasi cijeli sustav
+# Define a callback function on_off that toggles the ButtonState global variable when the button_on_off is pressed
 def on_off(button_on_off):
     global ButtonState
     ButtonState += 1
@@ -41,17 +42,19 @@ GPIO.add_event_detect(button_on_off, GPIO.FALLING, callback=on_off, bouncetime=2
 
 ButtonState1 = 0
 GPIO.setup(button_dist, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
+ # Define a callback function dist that toggles the ButtonState1 global variable when the button_dist is pressed
 def dist(button_dist):
     global ButtonState1
     ButtonState1 += 1
     if ButtonState > 1:
         ButtonState1 = 0
-    
+
+# Register the callback function for the falling edge detection of button_dist using GPIO.add_event_detect
 GPIO.add_event_detect(button_dist, GPIO.FALLING, callback=dist, bouncetime=200)
+# Perform a delay of 0.2 seconds for stability.
 time.sleep(0.2)
 
-#provjera rada ledica kada se uđe u program
+# Perform LED checks by briefly turning on and off the LEDs
 GPIO.output(zuta, GPIO.HIGH)
 GPIO.output(zelena, GPIO.HIGH)
 GPIO.output(crvena, GPIO.HIGH)
@@ -70,7 +73,7 @@ GPIO.setup(button1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(button2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(analog_pin, GPIO.IN)
 
-
+# rc_time function measures the time taken for a capacitor connected to the analog_pin to discharge through a photoresistor
 def rc_time (analog_pin):
     count = 0
 
@@ -193,6 +196,7 @@ try:
             upperBoarder = 0
             print("Sustav: deaktiviran, Senzor svjetla: ", upperBorder, "Granica osvjtljenja: ", granica)
             time.sleep(1)
-
+            
+# Exception Handling: Catches the KeyboardInterrupt exception to gracefully exit the program when Ctrl+C is pressed
 except KeyboardInterrupt:
     pass
